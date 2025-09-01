e0feb9b773ba1aaebf40045b00b22682e1be66bc
ISADORA DE SOUZA MIRANDA <isadora.3969@aluno.pr.senac.br>
2025-09-01 14:47:47 -0300

    Atribuindo propriedades, componentes e biblioteca de icones, para acesso das diferentes plataformas

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,6 +1,6 @@
 import React, { forwardRef, Fragment} from "react";
 import { View, Text, TextInput, TextInputProps } from "react-native";
-import { style } from "../../pages/login/styles";
+import { style } from "./style";
 // import { MaterialIcons } from '@expo/vector-icons';
 import { themas } from "../../global/themes";
 import { FontAwesome, MaterialIcons, Octicons} from '@expo/vector-icons';
@@ -21,12 +21,12 @@ type Props = TextInputProps & {
 
 export const Input = forwardRef(() => {
     return (
-        <>
+        <Fragment>
         <Text style={style.titleInput}>ENDEREÇO DE E-MAIL</Text>
         <View style={style.boxInput}>
             <TextInput style={style.input} />
             <MaterialIcons name="email" size={20} color={themas.colors.gray} />
         </View>
-        </>
+        </Fragment>
     )
 })
