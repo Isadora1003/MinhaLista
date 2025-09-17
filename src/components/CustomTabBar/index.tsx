ff7e82e70b41922885f52498ede130c7a8801f79
ISADORA DE SOUZA MIRANDA <isadora.3969@aluno.pr.senac.br>
2025-09-17 08:52:08 -0300

    Criando AuthContext, aplicando modal nas duas telas de list e user

--- src/components/CustomTabBar/index.tsx
+++ src/components/CustomTabBar/index.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";{}
 import { Text, TouchableOpacity, View } from "react-native";
 import { style } from "./styles";
 import {AntDesign, FontAwesome, Entypo, MaterialIcons} from '@expo/vector-icons';
 import { themas } from "../../global/themes";
+import { AuthContextList } from "../../context/authContext_list";
 
 export default ({state, navigation}: any) => {
+
+    const { onOpen } = useContext<any>(AuthContextList)
 
     const go = (screenName: string) => {
         navigation.navigate(screenName)
@@ -15,7 +18,7 @@ export default ({state, navigation}: any) => {
             <TouchableOpacity style={style.tabItem} onPress={() => go ("List")}>
                 <AntDesign name="bars" style={{ opacity: state.index === 0 ? 1: 0.3, color:themas.colors.primary, fontSize: 32}}/>
             </TouchableOpacity>
-            <TouchableOpacity style={style.tabItemButton}>
+            <TouchableOpacity style={style.tabItemButton} onPress={() => onOpen()}>
                 <View style={{width: '100%', left: 10, top: 4}}>
                     <Entypo name="plus" size={40} color={'#FFF'}/>
                 </View>
