bf56238694afee93b332d7776fac16cf25cfcb73
ISADORA DE SOUZA MIRANDA <isadora.3969@aluno.pr.senac.br>
2025-09-26 12:13:04 -0300

    Criando selected para flags, atribuindo valor booleano a ela, criando tambem a funcao close para o modal

--- src/context/authContext_list.tsx
+++ src/context/authContext_list.tsx
@@ -1,5 +1,5 @@
-import React, { createContext, useContext, useRef, useEffect} from "react";
-import { Dimensions, Text, View, StyleSheet, TouchableOpacity} from "react-native";
+import React, { createContext, useContext, useRef, useEffect } from "react";
+import { Dimensions, Text, View, StyleSheet, TouchableOpacity } from "react-native";
 import { MaterialIcons, AntDesign } from '@expo/vector-icons';
 import { Modalize } from "react-native-modalize";
 import { Input } from "../components/input";
@@ -22,15 +22,23 @@ export const AuthProviderList = (props: any): any => {
 
     }
 
+    const onClose = () => {
+        modalizeRef?.current?.close();
+    }
+
     useEffect(() => {
         onOpen()
-    },[])
+    }, [])
 
     const _renderFlags = () => {
-        return(
-            flags.map ((item, index) =>(
+        return (
+            flags.map((item, index) => (
                 <TouchableOpacity>
-                    <Flag caption={item.caption} color={item.color}/>
+                    <Flag
+                        caption={item.caption}
+                        color={item.color}
+                        selected 
+                    />
                 </TouchableOpacity>
             ))
         )
@@ -40,20 +48,20 @@ export const AuthProviderList = (props: any): any => {
         return (
             <View style={styles.container}>
                 <View style={styles.header}>
-                    <TouchableOpacity>
-                        <MaterialIcons name="close" size={30}/>
+                    <TouchableOpacity onPress={() => onClose()}>
+                        <MaterialIcons name="close" size={30} />
                     </TouchableOpacity>
                     <Text style={styles.title}>Criar Tarefa</Text>
                     <TouchableOpacity>
-                        <AntDesign name="check" size={30}/>
+                        <AntDesign name="check" size={30} />
                     </TouchableOpacity>
                 </View>
                 <View style={styles.content}>
-                    <Input title="Título" labelStyle={styles.label}/>
-                    <Input title="Descrição" labelStyle={styles.label} height={100} multiline numberOfLines={5}/>
+                    <Input title="Título" labelStyle={styles.label} />
+                    <Input title="Descrição" labelStyle={styles.label} height={100} multiline numberOfLines={5} />
                 </View>
-                <View style={{width: '40%'}}>
-                    <Input title="Tempo limite" labelStyle={styles.label}/>
+                <View style={{ width: '40%' }}>
+                    <Input title="Tempo limite" labelStyle={styles.label} />
                 </View>
                 <View style={styles.containerFlag}>
                     <Text style={styles.label}>Flags:</Text>
@@ -68,12 +76,12 @@ export const AuthProviderList = (props: any): any => {
         <AuthContextList.Provider value={{ onOpen }}>
             {props.children}
             <Modalize
-            ref={modalizeRef}
-            // modalHeight={Dimensions.get('window').height / 1.3}
-            childrenStyle = {{height: Dimensions.get('window').height / 1.3}}
-            adjustToContentHeight={true}
+                ref={modalizeRef}
+                // modalHeight={Dimensions.get('window').height / 1.3}
+                childrenStyle={{ height: Dimensions.get('window').height / 1.3 }}
+                adjustToContentHeight={true}
             >
-            {_container()}
+                {_container()}
             </Modalize>
         </AuthContextList.Provider>
     )
@@ -81,37 +89,37 @@ export const AuthProviderList = (props: any): any => {
 
 export const useAuth = () => useContext(AuthContextList);
 export const styles = StyleSheet.create({
-        container: {
-            width: '100%'
-        },
-        header: {
-            width: '100%',
-            height: 40,
-            paddingHorizontal: 40,
-            flexDirection: 'row',
-            marginTop: 20,
-            justifyContent: 'space-between',
-            alignItems: 'center'
-        },
-        title: {
-            fontSize: 20,
-            fontWeight: 'bold',
-        },
-        content: {
-            width: '100%',
-            paddingHorizontal: 20,
-        },
-        containerFlag: {
-            width: '100%',
-            padding: 10
-        },
-        label: {
-            fontWeight: 'bold',
-            color: '#000'
-        },
-        rowFlags: {
-            flexDirection: 'row',
-            gap: 10,
-            marginTop: 10
-        }
+    container: {
+        width: '100%'
+    },
+    header: {
+        width: '100%',
+        height: 40,
+        paddingHorizontal: 40,
+        flexDirection: 'row',
+        marginTop: 20,
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    content: {
+        width: '100%',
+        paddingHorizontal: 20,
+    },
+    containerFlag: {
+        width: '100%',
+        padding: 10
+    },
+    label: {
+        fontWeight: 'bold',
+        color: '#000'
+    },
+    rowFlags: {
+        flexDirection: 'row',
+        gap: 10,
+        marginTop: 10
+    }
 })
